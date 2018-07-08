4a29d0f1d4d92800e1f44d319d13fc44c803d990
Alice T <alicewtien@gmail.com>
2018-07-08 03:42:19 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -11,6 +11,8 @@ end
 def position_taken?(board, index)
   if index.between?(0, 8) && (board[index] == "X" || board[index] == "O")
     return true
+  elsif index.between?(0, 8) == false
+    return true
   else
     return false
   end
